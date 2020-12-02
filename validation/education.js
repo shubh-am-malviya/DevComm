fe53142ca07eaceb7c967fc7c69a0b0da3e8e699
Shubham <shubham24999@gmail.com>
2020-12-02 23:41:58 +0530

    Complete Add Education in Dashboard

--- validation/education.js
+++ validation/education.js
@@ -2,31 +2,31 @@ const Validator = require('validator');
 const isEmpty = require('./is-empty');
 
 module.exports = function validateEducationInput(data) {
-  let errors = {};
+	let errors = {};
 
-  data.school = isEmpty(data.school) ? '' : data.school;
-  data.degree = isEmpty(data.degree) ? '' : data.degree;
-  data.from = isEmpty(data.from) ? '' : data.from;
-  data.fieldofstudy = isEmpty(data.fieldofstudy) ? '' : data.fieldofstudy;
+	data.school = isEmpty(data.school) ? '' : data.school;
+	data.degree = isEmpty(data.degree) ? '' : data.degree;
+	data.from = isEmpty(data.from) ? '' : data.from;
+	data.fieldofstudy = isEmpty(data.fieldofstudy) ? '' : data.fieldofstudy;
 
-  if (Validator.isEmpty(data.school)) {
-    errors.title = 'School field is required';
-  }
+	if (Validator.isEmpty(data.school)) {
+		errors.school = 'School field is required';
+	}
 
-  if (Validator.isEmpty(data.degree)) {
-    errors.degree = 'Degree field is required';
-  }
+	if (Validator.isEmpty(data.degree)) {
+		errors.degree = 'Degree field is required';
+	}
 
-  if (Validator.isEmpty(data.from)) {
-    errors.from = 'From date field is required';
-  }
+	if (Validator.isEmpty(data.from)) {
+		errors.from = 'From date field is required';
+	}
 
-  if (Validator.isEmpty(data.fieldofstudy)) {
-    errors.fieldofstudy = 'Field of study is required';
-  }
+	if (Validator.isEmpty(data.fieldofstudy)) {
+		errors.fieldofstudy = 'Field of study is required';
+	}
 
-  return {
-    errors,
-    isValid: isEmpty(errors)
-  };
+	return {
+		errors,
+		isValid: isEmpty(errors),
+	};
 };
